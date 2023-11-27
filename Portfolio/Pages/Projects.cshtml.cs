b5eabc4f480c6a5c112fe87f908e5840772df16d
Filipe Brito <madfilipe@hotmail.com>
2023-11-27 16:00:45 -0500

    Projects page Font added

--- Portfolio/Pages/Projects.cshtml.cs
+++ Portfolio/Pages/Projects.cshtml.cs
@@ -1,10 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Shared.Models;
+
 
 namespace Portfolio.Pages
 {
     public class ProjectsModel : PageModel
     {
+        public List<Project> Projects { get; set; } = new List<Project>();
+
+        public ProjectsModel()
+        {
+            Projects.Add(new Project(){
+                Name = "Portfolio",
+                Description = "This website! A portfolio to showcase my projects and experience.",
+                Technologies = new string[] {
+                    "dotnet", "c#", "razor", "html", "css", "javascript", "tailwindcss"
+                    },
+                Url = "https://filipebrito.me/",
+                Source = "Github",
+                SourceUrl = "https://github.com/fnbrito/Portfolio",
+                ImageUrl = "Portfolio.png",
+                ImageDescription = "Portfolio",
+            });
+        }
+
         public void OnGet()
         {
         }
---
+++ Portfolio/Pages/Shared/Models/Project.cs
@@ -0,0 +1,37 @@
+﻿namespace Portfolio.Shared.Models;
+
+public class Project
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string ImageDescription { get; set; }
+    public string Url { get; set; }
+    public string Source { get; set; }
+    public string SourceUrl { get; set; }
+    public string[] Technologies { get; set; }
+
+    public Project()
+    {
+        Name = string.Empty;
+        Description = string.Empty;
+        ImageUrl = string.Empty;
+        ImageDescription = string.Empty;
+        Url = string.Empty;
+        Source = string.Empty;
+        SourceUrl = string.Empty;
+        Technologies = Array.Empty<string>();
+    }
+
+    public Project(string name, string description, string image, string url, string imageUrl, string imageDescription, string source, string sourceUrl, string[] technologies)
+    {
+        Name = name;
+        Description = description;
+        ImageUrl = imageUrl;
+        ImageDescription = imageDescription;
+        Url = url;
+        Source = source;
+        SourceUrl = sourceUrl;
+        Technologies = technologies;
+    }
+}
