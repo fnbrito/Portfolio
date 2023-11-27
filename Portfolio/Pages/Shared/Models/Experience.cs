002e699911a24cc7277696c92903b990a0c0743c
Filipe Brito <madfilipe@hotmail.com>
2023-11-27 16:00:17 -0500

    Minor adjustments Comments removal

--- Portfolio/Pages/Shared/Models/Experience.cs
+++ Portfolio/Pages/Shared/Models/Experience.cs
@@ -2,8 +2,8 @@
 
 public class Experience
 {
-  public string? Title { get; set; }
-  public string? Place { get; set; }
+  public string Title { get; set; }
+  public string Place { get; set; }
   public string[] Tags { get; set; }
   public string Location { get; set; }
   public string Url { get; set; }
@@ -13,4 +13,34 @@ public class Experience
   public DateTime EndDate { get; set; }
   public string Description { get; set; }
   public string[] BulletPoints { get; set; }
+
+  public Experience()
+  {
+    Title = string.Empty;
+    Place = string.Empty;
+    Tags = Array.Empty<string>();
+    Location = string.Empty;
+    Url = string.Empty;
+    ImageUrl = string.Empty;
+    ImageDescription = string.Empty;
+    StartDate = DateTime.MinValue;
+    EndDate = DateTime.MinValue;
+    Description = string.Empty;
+    BulletPoints = Array.Empty<string>();
+  }
+
+  public Experience(string title, string place, string[] tags, string location, string url, string imageUrl, string imageDescription, DateTime startDate, DateTime endDate, string description, string[] bulletPoints)
+  {
+    Title = title;
+    Place = place;
+    Tags = tags;
+    Location = location;
+    Url = url;
+    ImageUrl = imageUrl;
+    ImageDescription = imageDescription;
+    StartDate = startDate;
+    EndDate = endDate;
+    Description = description;
+    BulletPoints = bulletPoints;
+  }
 }
