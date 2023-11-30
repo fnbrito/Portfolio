1c64469dfccb737044caf42f188dd2f78d98df31
Filipe Brito <madfilipe@hotmail.com>
2023-11-30 16:32:49 -0500

    Made the SVG dynamic with a model

---
+++ Portfolio/Pages/Shared/Models/Character.cs
@@ -0,0 +1,25 @@
+﻿namespace Portfolio.Shared.Models;
+
+public class Character
+{
+    public string Width { get; set; }
+    public string Height { get; set; }
+    public string ViewBox { get; set; }
+    public string Margin { get; set; }
+
+    public Character()
+    {
+        Width = "25rem";
+        Height = "25rem";
+        ViewBox = "0 0 100 100";
+        Margin = "0 0 0 0";
+    }
+
+    public Character(string width, string height, string viewBox, string margin)
+    {
+        Width = width;
+        Height = height;
+        ViewBox = viewBox;
+        Margin = margin;
+    }
+}
